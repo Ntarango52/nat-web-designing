dd48b6f42e72c1eb21063882a6595b51aa2354fe
Nohemi Tarango <Nohemi5270@gmail.com>
2020-03-18 13:57:08 -0600

    added the persona description

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Milestone 1</title>
+</head>
+<body>
+<h1>Milestone 1</h1>
+</body>
+<body>
+<p>Purpose: I would like my portfolio to help me get a job by displaying my work on there and it impressing employers.</p>
+</body>
+<body>
+<p>Audience: My audience will be potential employers and clients.</p>
+</body>
+<body>
+<p>Goal: My goal for my portfolio is for it to serve as a gallery to showcase and promote my work.</p>
+</body>
+<body>
+<h1>Persona</h1>
+</body>
+<body>
+<h2>Bea Montgomery</h2>
+</body>
+<body>
+<p>Bea Montgomery is a 25 year old Mexican American female. Bea Montgomery is a new possible client that is looking for a web developer to build her a Website. She owns a bakery and needs a Website to showcase it and to show what makes her bakery unique. She has some experience with technology. For example, she has a Galaxy S9 that she uses to take phone calls. She also has a Microsoft Surface Book 2 that she uses to check her reviews on yelp and to look up new recipes. If Bea likes my portfolio website then she can contact me using the website to set up a meeting.</p>
+</body>
+</html>
