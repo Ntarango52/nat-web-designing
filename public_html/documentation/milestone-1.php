a0a5c3c4be1b46eb2e0660f3ed68306577d2f135
Nohemi Tarango <Nohemi5270@gmail.com>
2020-03-26 15:15:54 -0600

    added more info to the persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,10 +7,16 @@
 <body>
 <h1>Milestone 1</h1>
 <p>Purpose: I would like my portfolio to help me get a job by displaying my work on there and it impressing employers.</p>
-<p>Audience: My audience will be potential employers and clients.</p>
-<p>Goal: My goal for my portfolio is for it to serve as a gallery to showcase and promote my work.</p>
+<p>Audience: My audience will be potential employers from tech companies and clients looking for a web developer.</p>
+<p>Goal: My main goal for my portfolio is for it to serve as a gallery to showcase and promote my work. The second goal for my portfolio is for employers and clients to get into contact with me using the contact form.</p>
 <h1>Persona</h1>
-<h2>Bea Montgomery</h2>
-<p>Bea Montgomery is a 25 year old Mexican American female. Bea Montgomery is a new possible client that is looking for a web developer to build her a Website. She owns a bakery and needs a Website to showcase it and to show what makes her bakery unique. She has some experience with technology. For example, she has a Galaxy S9 that she uses to take phone calls. She also has a Microsoft Surface Book 2 that she uses to check her reviews on yelp and to look up new recipes. If Bea likes my portfolio website then she can contact me using the website to set up a meeting.</p>
+<p>Name: Bea Montgomery</p>
+<p>Age: 25</p>
+<p>Profession: Owner of a Mexican inspired bakery in Atlanta. She is looking for a web designer to make her a website to showcase the uniqueness of her bakery.</p>
+<p>Technology: She uses her Galaxy S9 to answer and make phone calls. She also has a Microsoft Surface Book 2 that she uses to check her reviews on yelp and to look up new recipes.</p>
+<p>Attitudes and Behaviors: Bea doesn't have much time on her hands since she doesn't have many employees and has to do a lot herself. She is looking for someone who can build her site and manage it, it doesn't matter if the web designer is working remote or not.</p>
+<p>Frustrations and Needs: Bea doesn't care how much years of school or experience they have. If she likes their portfolio then she will hire them. She will also interview them before hiring them to get to know more about their personality and background.</p>
+<p>Goals: Bea wants to hire a web developer who will put as much passion into a project as she does.</p>
+<p>User Story: As a user, I need to determine whether a person is a good candidate for the web developer position.</p>
 </body>
 </html>
