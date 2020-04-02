d31bd890a81211ed0880783c8c1b53aa64150676
Nohemi Tarango <Nohemi5270@gmail.com>
2020-04-01 20:51:35 -0600

    added the mobile and web wire frames to milestone 2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Milestone 2</title>
+</head>
+<body>
+<h1>Milestone 2</h1>
+<h2>Content Strategy</h2>
+<p>Welcome: Hello! My name is Nohemi Tarango. I am a Software Developer.</p>
+<p>About Me: </p>
+<p>Portfolio: </p>
+<p>Contact: </p>
+<h1>Wireframes</h1>
+<h2>Mobile Layout</h2>
+<img src="New%20Wireframe%204.png" alt="picture"/>
+<h2>Full Screen Layout</h2>
+<img src="New%20Wireframe%205.png" alt="picture"/>
+</body>
+</html>
